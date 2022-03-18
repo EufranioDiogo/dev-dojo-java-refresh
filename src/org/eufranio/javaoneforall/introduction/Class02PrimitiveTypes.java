03bf45e2a4218faf679f20435d3534bd60cc5841
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-03-18 21:44:14 +0100

    chore: working with primitive types

---
+++ src/org/eufranio/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -0,0 +1,33 @@
+package org.eufranio.javaoneforall.introduction;
+
+/**
+ * Author: Eufránio Diogo
+ * Topic: Primitive Types
+ * Date: 2021-03-18
+ */
+public class Class02PrimitiveTypes {
+    public static void main(String[] args) {
+        // 8 Primitive Types: int double float boolean char byte short long
+        int age = 20; // 4 Bytes
+        double pi = 3.1415; // 8 Bytes
+        float salary = 1000; // 4 Bytes
+        boolean isMarried = true; // 1 Byte
+        char character = 'a'; // 2 Bytes
+        byte smallNumber = 1; // 1 Byte
+        short shortNumber = 1; // 2 Bytes
+        long veryLongNumber = 1234567890; // 8 Bytes
+
+
+
+        System.out.println("Age (int): " + age);
+        System.out.println("PI (double): " + pi);
+        System.out.println("salary(1/2) (float): " + salary);
+        System.out.println("Is Married (boolean): " + isMarried);
+        System.out.println("Character (char): " + character);
+        System.out.println("small Number (byte): " + smallNumber);
+        System.out.println("short Number (short): " + shortNumber);
+        System.out.println("Long Number (long): " + veryLongNumber);
+
+        System.out.println("Class 02 running");
+    }
+}
