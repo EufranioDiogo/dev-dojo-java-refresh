5c3b274975dbced305e7c674ff0bda0abcd108f1
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-03-18 10:42:30 +0100

    chore: getting started on java dev dojo course

---
+++ src/org/eufranio/javaoneforall/introduction/HelloDevDojo.java
@@ -0,0 +1,17 @@
+package org.eufranio.javaoneforall.introduction;
+
+/**
+ * Author: Eufránio Diogo
+ * Project: Gotting better on Java
+ * Date: 2021-03-18
+*/
+public class HelloDevDojo {
+    // I can tyope any thing here and it will be completed ignored because it's a comment
+    /*
+       Multiple line comment
+       Hello Code Lovers
+    */
+    public static void main(String[] args) {
+        System.out.println("Olá mundo");
+    }
+}
