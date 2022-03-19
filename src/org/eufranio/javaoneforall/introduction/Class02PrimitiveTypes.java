ff9f82c13adfd04fb118e26a8d3a3175fdfb8d86
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-03-19 08:18:01 +0100

    chore: Primite Types covered

--- src/org/eufranio/javaoneforall/introduction/Class02PrimitiveTypes.java
+++ src/org/eufranio/javaoneforall/introduction/Class02PrimitiveTypes.java
@@ -12,11 +12,14 @@ public class Class02PrimitiveTypes {
         double pi = 3.1415; // 8 Bytes
         float salary = 1000; // 4 Bytes
         boolean isMarried = true; // 1 Byte
-        char character = 'a'; // 2 Bytes
-        byte smallNumber = 1; // 1 Byte
+        char character = 'E'; // 2 Bytes
+        char characterAsANumber = 69;
+        char characterAsUnicode = '\u2602';
+        byte smallNumber = 2; // 1 Byte
         short shortNumber = 1; // 2 Bytes
         long veryLongNumber = 1234567890; // 8 Bytes
-
+        var address = "Terra Nova - Luanda - Angola";
+        String name = "Eufránio Diogo";
 
 
         System.out.println("Age (int): " + age);
@@ -24,10 +27,13 @@ public class Class02PrimitiveTypes {
         System.out.println("salary(1/2) (float): " + salary);
         System.out.println("Is Married (boolean): " + isMarried);
         System.out.println("Character (char): " + character);
+        System.out.println("Character As a Number (char): " + characterAsANumber);
+        System.out.println("Character As Unicode(char): " + characterAsUnicode);
         System.out.println("small Number (byte): " + smallNumber);
         System.out.println("short Number (short): " + shortNumber);
         System.out.println("Long Number (long): " + veryLongNumber);
-
+        System.out.println("Address (var): " + address);
+        System.out.println("Name (String): " + name);
         System.out.println("Class 02 running");
     }
 }
---
+++ src/org/eufranio/javaoneforall/introduction/Class03PrimitiveTypesExercise.java
@@ -0,0 +1,12 @@
+package org.eufranio.javaoneforall.introduction;
+
+public class Class03PrimitiveTypesExercise {
+    public static void main(String[] args) {
+        String name = "Eufránio Diogo";
+        String address = "Terra Nova - Luanda - Angola";
+        float salary = 1450000.95f;
+        String date = "2022-03-18";
+
+        System.out.println(name + ", living at " + address + " confirm that I received the salary of " + salary + " AOA on " + date);
+    }
+}
