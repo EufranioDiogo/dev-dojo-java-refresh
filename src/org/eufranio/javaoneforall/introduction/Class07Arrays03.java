02de6d5f5c4a0576d83d1e3a662ea88d892e0185
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-03-23 00:46:31 +0100

    chore: foreach

---
+++ src/org/eufranio/javaoneforall/introduction/Class07Arrays03.java
@@ -0,0 +1,12 @@
+package org.eufranio.javaoneforall.introduction;
+
+public class Class07Arrays03 {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{1, 2, 3};
+
+        for (int number :
+                numbers) {
+            System.out.println("Valor: " + number);
+        }
+    }
+}
