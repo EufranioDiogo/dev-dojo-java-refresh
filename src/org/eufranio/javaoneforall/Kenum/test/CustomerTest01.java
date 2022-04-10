7cab2cb73d16f16c4106e98111612d9a23eb10b1
EufranioDiogo <eufraniodiogo5@gmail.com>
2022-04-10 18:54:18 +0100

    chore: Learning about enuma

---
+++ src/org/eufranio/javaoneforall/Kenum/test/CustomerTest01.java
@@ -0,0 +1,21 @@
+package org.eufranio.javaoneforall.Kenum.test;
+
+import org.eufranio.javaoneforall.Kenum.domain.Customer;
+import org.eufranio.javaoneforall.Kenum.domain.CustomerType;
+import org.eufranio.javaoneforall.Kenum.domain.PaymentType;
+
+public class CustomerTest01 {
+    public static void main(String[] args) {
+        Customer customer01 = new Customer("William Suavez", CustomerType.COMPANY, PaymentType.DEBIT);
+        Customer customer02 = new Customer("Eufránio Diogo", CustomerType.INDIVIDUAL, PaymentType.CREDIT);
+
+        System.out.println(customer01);
+        System.out.println(customer02);
+        System.out.println(PaymentType.DEBIT.calculateDiscount(1000));
+
+        CustomerType customerType = CustomerType.getCustomerType("iNDIVIDUAL");
+
+        System.out.println(customerType);
+
+    }
+}
