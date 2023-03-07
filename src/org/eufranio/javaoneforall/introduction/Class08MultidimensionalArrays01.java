22ec480a6583621ff948e02afc68a2a415553cd5
EufranioDiogo <eufraniodiogo5@gmail.com>
2023-03-07 04:58:33 +0100

    chore: closing intro part

--- src/org/eufranio/javaoneforall/introduction/Class08MultidimensionalArrays01.java
+++ src/org/eufranio/javaoneforall/introduction/Class08MultidimensionalArrays01.java
@@ -1,9 +1,18 @@
 package org.eufranio.javaoneforall.introduction;
 
+import java.util.Arrays;
+
 public class Class08MultidimensionalArrays01 {
     public static void main(String[] args) {
-        int[][] matrix = new int[2][2];
+        int[][] matrix = new int[4][];
         short counter = 1;
+
+        matrix[0] = new int[6];
+        matrix[1] = new int[3];
+        matrix[2] = new int[4];
+        matrix[3] = new int[1];
+
+
 
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -11,11 +20,21 @@ public class Class08MultidimensionalArrays01 {
             }
         }
 
-        for(int[] row : matrix) {
-            for (int element : row) {
-                System.out.print(element + " ");
+        matrix[3] = new int[]{counter++, counter++, counter++, counter++};
+
+        printMatrix(matrix);
+
+        System.out.println();
+
+        printMatrix(new int[][]{{1,2,3}, {4, 5, 3, 6}});
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
